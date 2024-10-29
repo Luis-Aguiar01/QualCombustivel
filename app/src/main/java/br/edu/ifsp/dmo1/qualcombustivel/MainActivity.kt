7d51227c328dec02ca9f4166c9113c78897db7f8
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-10-29 16:32:53 -0300

    Término no design da main activity e início da implementação da lógica do aplicativo

--- app/src/main/java/br/edu/ifsp/dmo1/qualcombustivel/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/qualcombustivel/MainActivity.kt
@@ -1,11 +1,29 @@
 package br.edu.ifsp.dmo1.qualcombustivel
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputGasoline: EditText
+    private lateinit var inputEthanol: EditText
+    private lateinit var buttonCalc: Button
+    private lateinit var textResult: TextView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeVariables()
     }
+
+    private fun initializeVariables() {
+        inputGasoline = findViewById(R.id.input_gasoline)
+        inputEthanol = findViewById(R.id.input_ethanol)
+        buttonCalc = findViewById(R.id.button)
+        textResult = findViewById(R.id.text_result)
+    }
+
 }
