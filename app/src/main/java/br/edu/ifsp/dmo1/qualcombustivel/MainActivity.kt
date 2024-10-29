9ff6a76d5ed6835c44cc0292bd6b5933af9aaa0e
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-10-29 16:50:45 -0300

    Término da implementação da lógica principal do aplicativo

--- app/src/main/java/br/edu/ifsp/dmo1/qualcombustivel/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/qualcombustivel/MainActivity.kt
@@ -1,22 +1,26 @@
 package br.edu.ifsp.dmo1.qualcombustivel
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var inputGasoline: EditText
     private lateinit var inputEthanol: EditText
     private lateinit var buttonCalc: Button
     private lateinit var textResult: TextView
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeVariables()
+
+        buttonCalc.setOnClickListener(this)
     }
 
     private fun initializeVariables() {
@@ -25,5 +29,28 @@ class MainActivity : AppCompatActivity() {
         buttonCalc = findViewById(R.id.button)
         textResult = findViewById(R.id.text_result)
     }
+
+    @Override
+    override fun onClick(view: View) {
+        if (view == buttonCalc) {
+            showResult()
+        }
+    }
+
+    private fun showResult() {
+        val gasolineValue = getInputValueConverted(inputGasoline)
+        val ethanolValue = getInputValueConverted(inputEthanol)
+        val proportion = ethanolValue / gasolineValue
+
+        if (proportion > 0.70) {
+            textResult.text = getString(R.string.result_gasoline)
+            textResult.setTextColor(getColor(R.color.gasoline))
+        } else  {
+            textResult.text = getString(R.string.result_ethanol)
+            textResult.setTextColor(getColor(R.color.ethanol))
+        }
+    }
+
+    private fun getInputValueConverted(view: EditText) = view.text.toString().toDouble()
 
 }
